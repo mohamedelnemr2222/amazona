4f40d6474cca695522d43991050154fce006e82c
mohamed elnemr <m.elnemr2222@gmail.com>
2022-12-11 20:38:02 +0200

    012

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,10 @@
 import { useEffect, useReducer, useState } from "react";
-
 import axios from "axios";
 import logger from "use-reducer-logger";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Product from "../components/Product";
+import { Helmet } from "react-helmet-async";
 //import data from "../data";
 
 const reducer = (state, action) => {
@@ -19,13 +19,13 @@ const reducer = (state, action) => {
       return state;
   }
 };
+
 function HomeScreen() {
   const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
     products: [],
     loading: true,
     error: "",
   });
-
   //const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
@@ -43,6 +43,9 @@ function HomeScreen() {
   }, []);
   return (
     <div>
+      <Helmet>
+        <title>Amazona</title>
+      </Helmet>
       <h1>Featured Products</h1>
       <div className='products'>
         {loading ? (
